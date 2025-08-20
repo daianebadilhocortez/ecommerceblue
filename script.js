acd81b67e97cf7404306c81a0fcd76540be462b6
daianebadilhocortez <daianebadilho@gmail.com>
2025-08-19 21:00:16 -0300

    alterações img

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const produtos = [
     { id: 1, nome: "Camiseta Masculina Estampa Geométrica", preco: 89.90, imagem: "img/camiseta1.jpg" },
     { id: 2, nome: "Camiseta Masculina Lisa", preco: 69.90, imagem: "img/camiseta2.jpg" },
     { id: 3, nome: "Camiseta Masculina Manga Longa", preco: 99.90, imagem: "img/camiseta3.jpg" },
-    { id: 4, nome: "Camiseta Masculina Estampada Casual", preco: 79.90, imagem: "img/camiseta4.jpg" },
-    { id: 5, nome: "Camiseta Masculina Slim Fit", preco: 89.90, imagem: "img/camiseta5.jpg" },
-    { id: 6, nome: "Camiseta Masculina Polo", preco: 129.90, imagem: "img/camiseta6.jpg" },
-    { id: 7, nome: "Camiseta Masculina Básica", preco: 59.90, imagem: "img/camiseta7.jpg" },
-    { id: 8, nome: "Camiseta Masculina Estampa Surf", preco: 79.90, imagem: "img/camiseta8.jpg" },
+    { id: 4, nome: "Camiseta Masculina Polo", preco: 129.90, imagem: "img/camiseta4.jpg" },
   ];
   
   const productList = document.getElementById("productList");
